3f0bb8aa87bc1d6899f09072ea66248ff5fa85aa
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-13 16:30:53 +0500

    notifcations

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from '../components/FlatCards';
@@ -10,55 +12,73 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 //navigation
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 //screens
 import Home from './screen/Home';
 import Details from './screen/Details';
 import PasswordGenerator from './screen/PasswordGenerator';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (rn === 'Details') {
-              iconName = focused ? 'list' : 'list-outline';
-            } else if (rn === 'Password Generator') {
-              iconName = focused ? 'list' : 'list-outline';
-            }
-            return <Icon name={iconName} size={size} color={'blue'} />;
-          },
-        })}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Trending Products'}}
-        />
-
-        <Tab.Screen
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen
           name="Details"
-          component={Details}
           initialParams={{productId: 100}}
-          options={{title: 'Product Details'}}
+          component={Details}
         />
-
-        <Tab.Screen
+        <Drawer.Screen
           name="Password Generator"
           component={PasswordGenerator}
-          options={{title: 'Password Generator'}}
         />
-      </Tab.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+// <NavigationContainer>
+//   <Tab.Navigator
+//     initialRouteName="Home"
+//     screenOptions={({route}) => ({
+//       tabBarIcon: ({focused, color, size}) => {
+//         let iconName;
+//         let rn = route.name;
+
+//         if (rn === 'Home') {
+//           iconName = focused ? 'home' : 'home-outline';
+//         } else if (rn === 'Details') {
+//           iconName = focused ? 'list' : 'list-outline';
+//         } else if (rn === 'Password Generator') {
+//           iconName = focused ? 'list' : 'list-outline';
+//         }
+//         return <Icon name={iconName} size={size} color={'blue'} />;
+//       },
+//     })}>
+//     <Tab.Screen
+//       name="Home"
+//       component={Home}
+//       options={{title: 'Trending Products'}}
+//     />
+
+//     <Tab.Screen
+//       name="Details"
+//       component={Details}
+//       initialParams={{productId: 100}}
+//       options={{title: 'Product Details'}}
+//     />
+
+//     <Tab.Screen
+//       name="Password Generator"
+//       component={PasswordGenerator}
+//       options={{title: 'Password Generator'}}
+//     />
+//   </Tab.Navigator>
+// </NavigationContainer>
--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,9 +1,39 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import notifee, {AndroidImportance} from '@notifee/react-native';
 
 // navigation
 
 const Home = ({navigation}) => {
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    if (Platform.OS == 'ios') {
+      await notifee.requestPermission();
+    }
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default2',
+      name: 'Default Channel2',
+      sound: 'default',
+      importance: AndroidImportance.HIGH,
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        // smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.smallText}>Home Screen</Text>
@@ -11,6 +41,8 @@ const Home = ({navigation}) => {
         title="Go to Details"
         onPress={() => navigation.navigate('Details', {productId: 86})}
       />
+
+      <Button title="Display Notification" onPress={onDisplayNotification} />
     </View>
   );
 };
